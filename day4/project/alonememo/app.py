412257a9f5ac54dc4183a49efb8fe54b41c7641f
archkt <archkt@berkeley.edu>
2021-12-24 11:27:45 -0800

    [day5] meta crawling

--- day4/project/alonememo/app.py
+++ day4/project/alonememo/app.py
@@ -1,43 +1,49 @@
 from flask import *
 app = Flask(__name__)
 
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client.likelion
+import requests
+from bs4 import BeautifulSoup
 
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.likelion                      # 'likelion'라는 이름의 db를 만듭니다.
+
+## HTML을 주는 부분
 @app.route('/')
 def home():
    return render_template('index.html')
 
-@app.route('/submit', methods=['POST'])
-def submit_info():
-   # receive values from the form
-   form_name = request.form['input_name']
-   form_quantity = request.form['input_quantity']
-   form_address = request.form['input_address']
-   form_phone = request.form['input_phone']
+@app.route('/memo', methods=['GET'])
+def listing():
+    # 1. 모든 document 찾기 & _id 값은 출력에서 제외하기
+    result = list(db.articles.find({},{'_id':0}))
+    # 2. articles라는 키 값으로 영화정보 내려주기
+    
+    return jsonify({'result':'success', 'msg':'GET 연결되었습니다!', 'articles':result})
 
-   # make them as dictionary for MongoDB
-   order = {'name': form_name,
-   'quantity': form_quantity,
-   'address': form_address,
-   'phone': form_phone}
-   
-   # insert review
-   db.memo_review.insert_one(order)
+## API 역할을 하는 부분
+@app.route('/memo', methods=['POST'])
+def saving():
+	# 1. 클라이언트로부터 데이터를 받기
+    url = request.form['form_url']
+    comment = request.form['form_comment']
+	# 2. meta tag를 스크래핑하기
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url,headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
 
-   # jsonify object
-   json_object = jsonify({'result':'success', 'msg':'주문이 완료되었습니다'})
+    og_image = soup.select_one('meta[property="og:image"]')
+    og_title = soup.select_one('meta[property="og:title"]')
+    og_description = soup.select_one('meta[property="og:description"]')
 
-   return json_object
+    image = og_image['content']
+    title = og_title['content']
+    description = og_description['content']
+	# 3. mongoDB에 데이터 넣기
+    article = {'url':url, 'comment':comment, 'title': title, 'image': image, 'description':description}
+    db.articles.insert_one(article)
 
-@app.route('/submit', methods=['GET'])
-def receive_info():
-   # get order information from the db
-   order_list = list(db.memo_review.find({},{'_id':0}))
-   
-   return jsonify({'result':'success', 'msg': 'successfully loaded', 'reviews': order_list})
-   
-   
-if __name__ == '__main__':  
+    return jsonify({'result': 'success', 'msg':'POST 연결되었습니다!'})
+
+if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
